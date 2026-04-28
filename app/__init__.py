62ac5c62d8489cf91b198cc8528cfb87ec9901c3
garreth-jaspay <c24451694@mytudublin.ie>
2026-04-28 17:12:46 +0100

    Latest version of FindmyDorm website

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, session
+from flask import Flask, session, request
 from flask_babel import Babel
 
 #This will set the language variable in session for the Babel app
@@ -15,12 +15,8 @@ app = Flask(__name__)
 #We are going to use the session to manage language then we need to set a secret key
 app.secret_key='a secret key'
 
-#Hook Babel into your app
-babel = Babel(app)
-
 #Initiate the Babel app passing through the locale you want to start with
-babel.init_app(app, locale_selector=get_locale)
+babel = Babel(app, locale_selector=get_locale)
 
 #Import your routes (you need to configure these in routes.py)
 from app import routes
-
